ab7625d7ce02e59c0d42de9effe6ad1861ad9034
Ammar Oker <oker.ammar@gmail.com>
2022-04-06 14:26:15 +0300

    fix images Cumulative Layout Shift

--- components/Image/Image.js
+++ components/Image/Image.js
@@ -3,7 +3,7 @@ import * as S from './styles';
 export default function Image({ src, alt }) {
   return (
     <S.StyledFigure>
-      <S.StyledImage src={src} alt={alt} />
+      <S.StyledImage src={src} alt={alt} height="400px" />
     </S.StyledFigure>
   );
 }
